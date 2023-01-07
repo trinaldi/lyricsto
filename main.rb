e4fa8c9fc4888a8de01d971a4755822ebe9289c2
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-07 12:01:59 -0300

    Second iteration of the program

--- main.rb
+++ main.rb
@@ -1,56 +1,58 @@
 require 'open-uri'
 require 'nokogiri'
-require 'byebug'
 require './urlbuilder'
 
 include URLBuilder
 
 class LyricsTo
-  attr_reader :band, :song
+  attr_reader :artist, :song
 
-  def initialize(band:, song:)
-    @band = band
+  def initialize(artist:, song:)
+    @artist = artist
     @song = song
   end
 
-  def url
-    URLBuilder.full_url(@band, @song)
-  end
-
-  def html
-   Nokogiri::HTML5(URI.open(url))
-  end
-
-  def matchify(css, match)
-    css.text.match(/#{match}/i)
-  end
-
-  def band_name
-    band_name = html.css('b')
-    matchify(band_name, band)
+  def artist_name
+    matchify(html.css('b'), artist)
   end
 
   def song_title
-    song_title = html.css('b')
-    matchify(song_title, song)
+    matchify(html.css('b'), song)
   end
 
   def lyrics
-    html.xpath('//div[@class="ringtone"]//following-sibling::div[1]').text
+    words = get_lyrics
+    sentenceize(words).each { |word| puts word }
   end
 
-  def display_lyrics
-    sentenceize(lyrics).each { |l| puts l }
+  private
+
+  def url
+    URLBuilder.full_url(artist, song)
+  end
+
+  def get_lyrics
+    html.xpath('//div[@class="ringtone"]//following-sibling::div[1]').text
   end
 
   def sentenceize(text)
     text.split("\n")
   end
+
+
+  def matchify(css, match)
+    css.text.match(/#{match}/i)
+  end
+
+
+  def html
+    Nokogiri::HTML5(URI.open(url))
+  end
 end
 
-band = ARGV[0]
+artist = ARGV[0]
 song = ARGV[1]
-l = LyricsTo.new(band: band, song: song)
-l.band_name
+l = LyricsTo.new(artist: artist, song: song)
+l.artist_name
 l.song_title
-l.display_lyrics
+l.lyrics
