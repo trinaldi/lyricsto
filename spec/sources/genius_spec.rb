7ccb9bdfa21781fa257088a49ad6cb39c99a1d85
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-21 20:06:05 -0300

    Add tests for genius descriptions

--- spec/sources/genius_spec.rb
+++ spec/sources/genius_spec.rb
@@ -9,21 +9,22 @@ describe Sources::Genius do
     context 'when an artist and song are given' do
       let(:response) { described_class.new(artist: 'Beatles', song: 'Help') }
       let(:request) do
-        stub_request(:get, 'https://genius.com/The-beatles-help-sample')
+        stub_request(:get, 'https://genius.com/The-beatles-help-lyrics')
           .to_return(status: 200, body: read_file('genius_description.html'), headers: {})
       end
       let(:api_request) do
         stub_request(:get, 'https://genius.com/api/search/multi?per_page=1&q=Beatles%20Help')
-          .to_return(status: 200, body: '', headers: {})
+          .to_return(status: 200, body: read_file('genius_api_response.json'), headers: {})
       end
 
       before do
         api_request
         request
+        response
       end
 
       it 'returns the song url' do
-        expect(response.fetch_song_url).to eq(0)
+        expect { response.fetch_song_url }.to output(/Help!/i).to_stdout
       end
     end
   end
