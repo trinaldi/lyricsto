f66126f6fb20543e73ddab1da31e4d19e07ace2b
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-22 16:02:52 -0300

    Add spec for Souce module methods

--- sources/source.rb
+++ sources/source.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
-module Source
-  def spoof_user_agent
-    {
-      'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1)' \
-      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-    }
-  end
+module Sources
+  module Source
+    def spoof_user_agent
+      {
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1)' \
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+      }
+    end
 
-  def split_newline(text)
-    text.split("\n")
-  end
+    def split_newline(text)
+      text.split("\n")
+    end
 
-  def text_splitter(text, line_size = 80)
-    puts text.gsub(/(?:.{1,#{line_size}}|\S+)\K(?:$|\s)/, "\n")
-  end
+    def text_splitter(text, line_size = 80)
+      puts text.gsub(/(?:.{1,#{line_size}}|\S+)\K(?:$|\s)/, "\n")
+    end
 
-  def display_lyrics(words)
-    split_newline(words).each { |word| puts word }
+    def display_lyrics(words)
+      split_newline(words).each { |word| puts word }
+    end
   end
 end
---
+++ spec/sources/source_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../sources/source'
+
+describe Sources::Source do
+  let(:dummy) { Class.new.extend(described_class) }
+
+  specify '#spoof_user_agent' do
+    expect(dummy.spoof_user_agent).to include(match(/User-agent/i))
+  end
+
+  describe '#split_newline' do
+    context 'when given a string with the \n char' do
+      let(:text) { "Hello\nWorld" }
+      let(:text_array) { %w[Hello World] }
+
+      it 'will break it into an array' do
+        expect(dummy.split_newline(text)).to eq(text_array)
+      end
+    end
+  end
+
+  describe '#text_splitter' do
+    context 'when given a string longer than the preset line_size argument' do
+      let(:text) { 'I am the Walrus' }
+      let(:line_size) { text.length / 2 }
+
+      it 'will break it in line_number chars each line' do
+        expect { dummy.text_splitter(text, line_size) }.to output("I am\nthe\nWalrus\n").to_stdout
+      end
+    end
+  end
+
+  describe '#display_lyrics' do
+    context 'when given an array of string' do
+      let(:string_array) { 'Hello Goodbye' }
+      let(:text) { "Hello Goodbye\n" }
+
+      it 'will output each item to stdout' do
+        expect { dummy.display_lyrics(string_array) }.to output(text).to_stdout
+      end
+    end
+  end
+end
